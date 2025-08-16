c9f246fa81fed85f1f805cb0363f0204132cbbf7
syed musab <syedmusab585@gmail.com>
2025-08-16 21:21:35 +0500

    Own React Just getting how the react is working

---
+++ CustomReact/react.js
@@ -0,0 +1,21 @@
+// 
+const maincontainer=document.querySelector("#root");
+const reactelement={
+    type:'a',
+    props:{
+        href:'https://www.google.com',
+        target:'_blank'
+    },
+    children:'Click me to Visit google'
+}
+
+function customRender(reactElement, container) {
+    const domElement=document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+    container.appendChild(domElement);
+
+
+}
+customRender(reactelement,maincontainer);
